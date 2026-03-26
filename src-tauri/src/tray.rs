ccf0544fcc6c7b7bed4ec0172f114f4d4f2ca4a5
Lance <mr.longzhentian@gmail.com>
2026-03-26 11:58:51 +0800

    feat: on Windows, change the tray icon based on the theme.

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,9 +1,12 @@
 use super::theme;
 use tauri::{
+    image::Image,
     menu::{CheckMenuItem, Menu, MenuBuilder, MenuEvent, MenuItem, SubmenuBuilder},
     tray::{MouseButton, MouseButtonState, TrayIcon, TrayIconBuilder, TrayIconEvent},
     App, AppHandle, Error, Manager, Theme, Wry,
 };
+
+const TRAY_ID: &str = "main-tray";
 
 fn menu_init(app: &App) -> Result<Menu<Wry>, Error> {
     let dark = CheckMenuItem::with_id(app, "dark", "Dark", true, theme::dark(app), None::<&str>)?;
@@ -28,10 +31,12 @@ fn menu_init(app: &App) -> Result<Menu<Wry>, Error> {
         "dark" => {
             app_handle.set_theme(Some(Theme::Dark));
             light.set_checked(false).expect("Change check error");
+            tray_icon_change(app_handle, Theme::Dark);
         }
         "light" => {
             app_handle.set_theme(Some(Theme::Light));
             dark.set_checked(false).expect("Change check error");
+            tray_icon_change(app_handle, Theme::Light);
         }
         "quit" => {
             app_handle.exit(0);
@@ -68,15 +73,43 @@ fn tray_icon_event(tray: &TrayIcon, event: TrayIconEvent) {
     }
 }
 
+fn tray_icon_change(app_handle: &AppHandle, theme: Theme) {
+    #[cfg(target_os = "windows")]
+    {
+        let icon = if theme == Theme::Dark {
+            Image::from_bytes(include_bytes!("../icons/tray_black.png")).ok()
+        } else {
+            Image::from_bytes(include_bytes!("../icons/tray.png")).ok()
+        };
+
+        app_handle
+            .tray_by_id(TRAY_ID)
+            .expect("Failed to get tray")
+            .set_icon(icon)
+            .expect("Failed to change tray icon");
+    }
+}
+
 pub fn init(app: &mut App) -> Result<(), Error> {
     let menu = menu_init(app)?;
 
-    let _tray = TrayIconBuilder::new()
+    let tray = TrayIconBuilder::<Wry>::with_id(TRAY_ID)
         .icon(app.default_window_icon().unwrap().clone())
         .menu(&menu)
         .show_menu_on_left_click(false)
         .on_tray_icon_event(tray_icon_event)
         .build(app)?;
+
+    #[cfg(target_os = "windows")]
+    {
+        tray.icon(app.default_window_icon().unwrap().clone())
+    }
+
+    #[cfg(target_os = "macos")]
+    {
+        let icon = Image::from_bytes(include_bytes!("../icons/tray.png")).ok();
+        let _ = tray.set_icon(icon);
+    }
 
     Ok(())
 }
